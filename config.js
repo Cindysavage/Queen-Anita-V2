e8c986b1d960153c6d074b82a4d2d7395113dfe6
Cindysavage <savagecindy731@gmail.com>
2024-10-08 01:51:09 +0000

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3AD
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
 global.devs = "https://t.me/deecee_x";
 global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.owner = process.env.OWNER_NUMBER || "2250171691128";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -42,7 +42,7 @@ global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUVB4aDZsTWZXTGF4dlFGakJhYkZoc0wvYnpxd1RUc1ZLV3RWQjg1Z1luZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieHBvUkhYcFprWEUzQ25RQzFyRUhVZDFzc1lNcWNZZCt6UlRoOEpoMEhUQT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI2R3NJZXBrMHJSeXlKRWo5SlJHMU9OWGxjeERVOE40d0VMS0F0d3VBQjFBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJJM0VJalNxdVNQS25GOFRMWTRNcVR6STVKTG1jc1VqTitMR3pmUWhjQXlRPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVMLzhNM1Y3c2cvMnhQck54VHJJc1FHZHNucS9Nd0k2RHVzL1hEVC81MVk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImwwRFJKYUFvWDlyMVYxQkJuRXdaK2Rld1Bkem85SFJ0bXh3cFVrR29Rbms9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiaUtnVXdmTHArSjBuMElzN1oraUpPc2UxbGsxZ1RFQldZMUptRTJtUmpWOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYWhzeGttK3dHdXlvV2JCWXpNRCtrMDVQb2MwVWxLaFNyMjBxZFREbVRIcz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ikh6TUdhd3JCekdPM1FDdmt2T3pab0NEL25CaVdKbG0xU0xrbm5JTTZmL0pyWXkwQTF4Yjh6N1RwUko5WS9NSjRRWnB6RGVCS0EveGRaaDBkRUg1YWl3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTc2LCJhZHZTZWNyZXRLZXkiOiI0NCs2RjlUNy9XbXNYWVN0NUZVUSt1OGc5RC9xR2ZLVDF2SHV4amNaZHVzPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiIyaDdhVFBjaVJGNnp4amdETThVZy1RIiwicGhvbmVJZCI6ImNlZTA3ODBiLWE0YWQtNDEzZC1hNzAyLTg5ZTA1ZWU5ZDcyOSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIvem1qc0dSWjM2aUpFOTVWbGpneThRU3hMOWM9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMzQ5RFlJUitUYmNoTUZBdTIxL01SRERBdUkwPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IkZYMlkzNFpWIiwibWUiOnsiaWQiOiIyMjU3MTY5MTEyODo2OEBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTmJHMXBFR0VPWG9rYmdHR0FNZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiRGFkQkFhbzBHeUJLZ2xRMUZRcEE5dFZJcktHbHZLREozam1XRWxwTWJsQT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiOFQvNjUyQXpKdTh0NFNkeWxQN0doZ3YzY290TFA5Ym9oaEl6M2JNUDZwT2R0Ni96TFp6U3QvdjFEcWVGNGZvcHZsdG5jQkxSL21oZ1p1SlE2WnJEQkE9PSIsImRldmljZVNpZ25hdHVyZSI6IlBCYnk4SkNzdCtOWnQyR1NoWTYxQkVPK2RlbE5BaDREdDdPNEdLK1doUlNaQitDdWd2M0NhR25McTluOUNkYjVzLzh2S2o1RU9hVTNCRzZ1SnhZNGlRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjI1NzE2OTExMjg6NjhAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCUTJuUVFHcU5Cc2dTb0pVTlJVS1FQYlZTS3locGJ5Z3lkNDVsaEphVEc1USJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyODM0NTIwMn0="
 module.exports = {
   menu: process.env.MENU || "2",
   HANDLERS: process.env.PREFIX || ".",
@@ -52,7 +52,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
   packname: process.env.PACK_NAME || "A N I T A",
   botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  ownername: process.env.OWNER_NAME || "Queen tidy",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -63,7 +63,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
   LANG: (process.env.THEME || "A N I T A").toUpperCase(),
 };
 global.rank = "updated";
